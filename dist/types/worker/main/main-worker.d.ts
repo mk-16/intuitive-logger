0ec116ac26a27b74c6077297e8a63c2f3c431c02
mk-16 <michael.laloush@gmail.com>
2024-08-20 14:42:37 +0300

    [=]

--- dist/types/worker/main/main-worker.d.ts
+++ dist/types/worker/main/main-worker.d.ts
@@ -2,6 +2,6 @@ import { Log } from "../../utils/log/log.js";
 export declare class LoggerWorker {
     #private;
     static on(): void;
-    static postLog(log: Log): any;
+    static postLog(log: Log): void;
 }
 //# sourceMappingURL=main-worker.d.ts.map
--- dist/worker/main/main-worker.js
+++ dist/worker/main/main-worker.js
@@ -7,7 +7,12 @@ export class LoggerWorker {
                 import("../client/web-worker.js").then(({ url }) => {
                     this.#worker = new Worker(new URL(url), { 'name': "intuitive-logger-web-worker" });
                     for (const log of this.#buffer) {
-                        this.#worker.postMessage(log);
+                        try {
+                            this.#worker.postMessage(log);
+                        }
+                        catch (e) {
+                            this.#worker.postLog(JSON.parse(JSON.stringify(log)));
+                        }
                     }
                 });
             else
@@ -24,7 +29,13 @@ export class LoggerWorker {
     static on() { }
     static postLog(log) {
         if (this.#worker !== undefined) {
-            return this.#worker.postMessage(log);
+            try {
+                this.#worker.postMessage(log);
+            }
+            catch (e) {
+                this.#worker.postLog(JSON.parse(JSON.stringify(log)));
+            }
+            return;
         }
         this.#buffer.push(log);
     }
