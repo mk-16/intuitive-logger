473ffb1219b2550acded8dc2a2d0a6ff813d03d1
mk-16 <michael.laloush@gmail.com>
2024-08-20 18:17:24 +0300

    [=]

--- src/worker/server/worker-thread.ts
+++ src/worker/server/worker-thread.ts
@@ -1,23 +1,23 @@
 import { filter, fromEvent, map, tap } from "rxjs";
-import { isMainThread, parentPort } from "node:worker_threads";
+// import { isMainThread, parentPort } from "node:worker_threads";
 import { findFileInStack } from "../../utils/functions/findFileInStack.js";
 import { functionLogGuard } from "../../utils/log/log-guards.js";
 import { FunctionLog, Log } from "../../utils/log/log.js";
-if (!isMainThread && parentPort)
-    fromEvent<MessageEvent<Log>>(parentPort, "message")
-        .pipe(
-            map(event => event.data),
-            filter((log): log is FunctionLog => functionLogGuard(log)),
-            map(log => {
-                const runtime = (log.endTime ?? 0) - (log.startTime ?? 0);
-                delete log.startTime
-                delete log.endTime
-                log.runtime = runtime;
-                log.stack = log.stack ? findFileInStack(log.stack) : undefined
-                return log;
-            }),
-            tap(console.log)
-        )
-        .subscribe();
+// if (!isMainThread && parentPort)
+//     fromEvent<MessageEvent<Log>>(parentPort, "message")
+//         .pipe(
+//             map(event => event.data),
+//             filter((log): log is FunctionLog => functionLogGuard(log)),
+//             map(log => {
+//                 const runtime = (log.endTime ?? 0) - (log.startTime ?? 0);
+//                 delete log.startTime
+//                 delete log.endTime
+//                 log.runtime = runtime;
+//                 log.stack = log.stack ? findFileInStack(log.stack) : undefined
+//                 return log;
+//             }),
+//             tap(console.log)
+//         )
+//         .subscribe();
 
 export const url = import.meta.url;
