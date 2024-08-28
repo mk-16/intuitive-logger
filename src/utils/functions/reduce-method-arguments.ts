7e92db8262d843483b45aa146a5b5432d2c6806c
michael <michael.laloush@gmail.com>
2024-08-29 00:23:30 +0300

    [=]

--- src/utils/functions/reduce-method-arguments.ts
+++ src/utils/functions/reduce-method-arguments.ts
@@ -1,8 +1,10 @@
 export function reduceMethodArguments(params: string[], args: undefined | string[]) {
     const results: Record<string, unknown> = {};
     for (const index of params.keys()) {
-        if (params[index].includes('...') && args != undefined)
-            results[params[index].split('...')[1]] = JSON.parse('[]'); //args.slice[index]
+        if (params[index].includes('...') && args != undefined){
+            const  fs = args.slice(index)
+            results[params[index].split('...')[1]] = args.slice(index).map(element => JSON.parse(element)); //args.slice[index]
+        }
         else
             try {
                 results[params[index]] = args !== undefined ? JSON.parse(args[index]) : undefined;
